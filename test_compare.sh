661b73ed1146bfb4ae44845d6b9c51ddf33902d8
Wang ziyu <wangziyu@sjtu.edu.com>
2019-06-12 14:32:19 +0800

    add cpu utilization graph

--- test_compare.sh
+++ test_compare.sh
@@ -6,7 +6,6 @@ touch top.dat
 cd ..
 
 sed -i "s/# maxmemory <bytes>/maxmemory 64mb/g" ../tool/redis-5.0.5/redis_original.conf
-sed -i "s/# maxmemory-policy noeviction/maxmemory-policy allkeys-lru/g" ../tool/redis-5.0.5/redis.conf
 
 echo "1. Concurrent Connections-Throughput,Latency" >> ./plot_data/compare.dat
 echo "memcached"?>> ./plot_data/compare.dat
@@ -19,9 +18,8 @@ do
     ../tool/memcached-1.5.14/memcached -d -m 64 -u root -p 11211 -P /tmp/memcached.pid -t 20 
     sleep 1s
     nohup ../tool/memtier_benchmark/memtier_benchmark -p 11211 -P memcache_binary -t $i -n 20000 --ratio=1:1 &
-    sleep 3s
-    top -b -n 1 | grep 'memcached'>> ./plot_data/top.dat
-    sleep 30s
+    top -b -n 30 | grep 'memcached'>> ./plot_data/top.dat
+    # sleep 30s
     kill `cat /tmp/memcached.pid`
     sleep 1s
 
@@ -36,9 +34,8 @@ do
     ../tool/redis-5.0.5/src/redis-server ../tool/redis-5.0.5/redis_original.conf &
     sleep 1s
     nohup ../tool/memtier_benchmark/memtier_benchmark -t $i -n 20000 --ratio=1:1 &
-    sleep 3s
-    top -b -n 1 | grep 'redis' >> ./plot_data/top.dat
-    sleep 30s
+    top -b -n 30 | grep 'redis' >> ./plot_data/top.dat
+    # sleep 30s
     ../tool/redis-5.0.5/src/redis-cli shutdown
     sleep 1s
 done
